7ebed33c2362477402847eaa4bf0c6f73fd16f19
Alice Julia <allywond@gmail.com>
2022-09-12 22:24:11 -0300

    first commit

---
+++ src/pages/Home/home.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function Home() {
+    const image = require('../../../assets/logo-brasileirão.png');
+
+    //const onPressRound = () =>  }
+
+
+    return (
+        <View>
+            <LinearGradient style={style.linearGradient} start={[0.4, 1.5]} end={[0, 0]} colors={['#008000', 'transparent']} />
+            <Image source={image} resizeMode='center' style={{ width: 400, heigh: 400 }} />
+            <TouchableOpacity >
+                <Text style={style.textButton}>
+                    Rodada atual
+                </Text>
+            </TouchableOpacity>
+
+        </View>
+    )
+
+}
+
+const style = StyleSheet.create({
+    linearGradient: {
+        position: 'absolute',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
+    },
+    textButton: {
+        color: 'rgb( 128, 128, 128,)',
+        fontsize: 20
+    }
+})
