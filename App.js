cf00e1e6b6073f5bc6fb02d825f07ba417b6270c
Alice <Allywond@gmail.com>
2022-09-18 23:30:10 -0300

    Drawer

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import DrawerRoute from './src/pages/routes/drawer/index.js';
-import HomePage from '../React/src/pages/Home/home.js'
+import Home from '../React/src/pages/Home/home.js'
 
 export default function App() {
   return (
-    <HomePage />
+    <Home />
   );
 }
 
